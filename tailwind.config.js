eaefbbae187186e9949b171858c71fd755c214aa
Gabriel <soramoretto@gmail.com>
2022-06-11 03:16:19 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+  purge: {
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+    ],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from-/
+    ]
+  },
+
   theme: {
     extend: {},
   },
