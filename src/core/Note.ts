12e18b87cfffd1b62d395adaf529ac9337ff6f86
Gabriel <soramoretto@gmail.com>
2022-06-11 01:10:42 -0300

    Create Note.ts

---
+++ src/core/Note.ts
@@ -0,0 +1,31 @@
+export default class Note {
+    #id: number
+    #title: string
+    #content: string
+
+    constructor(title: string, content: string, id: number = null) {
+        this.#title = title
+        this.#content = content
+        this.#id = id
+    }
+
+    get id() {
+        return this.#id
+    }
+
+    get title(){
+        return this.#title
+    }
+    
+    get content(){
+        return this.#content
+    }
+
+    set title(title){
+        this.#title = title
+    }
+
+    set content(content){
+        this.#content = content
+    }
+}
