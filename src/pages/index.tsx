3eb0b3be595ea86f4c41f4234db13ce2194f11a4
Gabriel <soramoretto@gmail.com>
2022-06-11 14:47:15 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,66 +2,35 @@ import MainSection from '../components/MainSection'
 import Layout from '../components/Layout'
 import CreateNote from '../components/CreateNote'
 import NoteTable from '../components/NoteTable'
-import Note from '../core/Note'
 import NoteForm from '../components/NoteForm'
-import { useEffect, useState } from 'react'
-import NoteCollection from '../server/database/noteCollection'
+import useNotes from '../hooks/useNotes'
 
 
 export default function Home() {
-  const repo: NoteCollection = new NoteCollection()
+  const {
+    editNote, 
+    deleteNote, 
+    note, 
+    notes, 
+    newNote, 
+    saveNote, 
+    mainShowed, 
+    showMain
+  } = useNotes()
 
-  const [show, setShow] = useState<'main' | 'form'>('main')
-  const [notes, setNotes] = useState<Note[]>([])
-  const [note, setNote] = useState<Note>(Note.empty())
-
-  useEffect(() => {
-    getAllNotes()
-  }, [])
-
-  function getAllNotes() {
-    repo.getAll().then((notes) => {
-      setNotes(notes)
-      setShow('main')
-    })
-  }
-
-  function editNote(note: Note){
-    setNote(note)
-    setShow('form')
-  }
-
-  async function deleteNote(note: Note){
-    await repo.delete(note)
-    getAllNotes()
-  }
-
-  function newNote(){
-    setNote(Note.empty())
-    setShow('form')
-  }
-
-  async function saveNote(note: Note){
-    await repo.save(note)
-    getAllNotes()
-  }
-  //<MainSection>
-  //<CreateNote>+</CreateNote>
-  //<NoteTable notes={array} selectedNote={editNote} deletedNote={deleteNote}></NoteTable>
-  //</MainSection>
   return (
     <div className={`
       flex min-h-screen justify-center items-center
       bg-gradient-to-r from-blue-500 to-purple-500
     `}>
       <Layout title='Notepadder'>
-        {show === 'main' ? (
+        {mainShowed ? (
           <MainSection>
             <CreateNote onClick={newNote}>+</CreateNote>
             <NoteTable notes={notes} selectedNote={editNote} deletedNote={deleteNote}></NoteTable>
           </MainSection>
         ) : (
-          <NoteForm note={note} cancelled={() => setShow('main')} noteChanged={saveNote}></NoteForm>
+          <NoteForm note={note} cancelled={() => showMain} noteChanged={saveNote}></NoteForm>
         )}
       </Layout>
     </div>
